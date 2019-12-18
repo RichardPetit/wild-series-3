6bb11cc53722d5edfb62c3d8df619f354adade0c
Richard Petit <richard-petit@live.fr>
2019-12-18 10:39:07 +0100

    entityType done

--- src/Form/ActorType.php
+++ src/Form/ActorType.php
@@ -13,7 +13,7 @@ class ActorType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('programs')
+            ->add('programs', null, ['choice_label' => "title"])
         ;
     }
 
